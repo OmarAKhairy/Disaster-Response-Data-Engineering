fb2eccf3e38bd8982e75aef8ac8f50d3d3beb4b1
Omar Khairy <omarkhairy@outlook.com>
2024-03-24 11:47:40 +0200

    Update process_data.py to fix a bug in saving data to an already existing table
    The issue is fixed by adding  if_exists='replace' to the df.to_sql() to replace the existing table

--- data/process_data.py
+++ data/process_data.py
@@ -48,7 +48,7 @@ def save_data(df, database_filename):
     :return:
     '''
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('Messages', engine, index=False)  
+    df.to_sql('Messages', engine, index=False, if_exists='replace')  
 
 
 def main():
